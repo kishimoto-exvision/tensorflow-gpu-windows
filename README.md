cbe8d0a93fdda0025e2c0b201acecc9f61e6b5ad
kishimoto-exvision <kishimoto@exvision.co.jp>
2017-10-08 22:39:01 +0900

    Update tensorflow-gpu-windows/CMakeTensorFlow.cmd

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # tensorflow-gpu-windows
+
+## References
+
+ * <https://www.tensorflow.org/install/install_windows>
+ * <https://github.com/tensorflow/tensorflow/blob/master/tensorflow/contrib/cmake/README.md>
+ * <http://twonightmare.blogspot.jp/2017/08/tensorflow-gpu-build-for-windows-for.html>
+
+## How to build
+
+ * Download the prebuilt swig.exe from <http://prdownloads.sourceforge.net/swig/swigwin-3.0.12.zip> and so on.
+ * Add the PATH to swig.exe, and FindSWIG.cmake will find the path of swig.exe.
+ * Call the following commands.
+ 
+```
+cd .\tensorflow-gpu-windows
+.\CMakeTensorFlow.cmd
+```
--- tensorflow-gpu-windows/CMakeTensorFlow.cmd
+++ tensorflow-gpu-windows/CMakeTensorFlow.cmd
@@ -6,7 +6,13 @@ REM http://twonightmare.blogspot.jp/2017/08/tensorflow-gpu-build-for-windows-for
 REM Download the prebuilt swig.exe from http://prdownloads.sourceforge.net/swig/swigwin-3.0.12.zip and add the PATH to swig.exe, and FindSWIG.cmake will find the path of swig.exe.
 
 cd /d %~dp0
-copy /Y ".\tensorflow\contrib\cmake\CMakeLists.txt" "..\tensorflow\tensorflow\contrib\cmake\CMakeLists.txt"
+
+if not exist tensorflow (
+  git clone --recursive git://github.com/tensorflow/tensorflow "..\tensorflow"
+  git checkout 2c3bf9eff79156e32512e8d6da2179cd044167b8
+  copy /Y ".\tensorflow\contrib\cmake\CMakeLists.txt" "..\tensorflow\tensorflow\contrib\cmake\CMakeLists.txt"
+)
+
 cd "..\tensorflow\tensorflow\contrib\cmake"
 
 REM rmdir /S /Q build
@@ -15,23 +21,28 @@ cd build
 
 cmake .. -G "Visual Studio 14 2015 Win64" ^
 -Dtensorflow_ENABLE_GPU:BOOL=TRUE ^
+-DCMAKE_INSTALL_PREFIX:PATH="C:/SDKs/tensorflow-gpu-1.3"
 -DCUDNN_HOME:PATH="C:/Program Files/NVIDIA GPU Computing Toolkit/CUDA/v8.0" ^
 -Dtensorflow_BUILD_CC_EXAMPLE:BOOL=TRUE ^
--Dtensorflow_BUILD_CC_TESTS:BOOL=FALSE ^
--Dtensorflow_BUILD_PYTHON_BINDINGS:BOOL=FALSE ^
--Dtensorflow_BUILD_PYTHON_TESTS:BOOL=FALSE ^
+-Dtensorflow_BUILD_CC_TESTS:BOOL=TRUE ^
+-Dtensorflow_BUILD_PYTHON_BINDINGS:BOOL=TRUE ^
+-Dtensorflow_BUILD_PYTHON_TESTS:BOOL=TRUE ^
 -Dtensorflow_BUILD_SHARED_LIB:BOOL=FALSE ^
+-Dtensorflow_OPTIMIZE_FOR_NATIVE_ARCH:BOOL=TRUE ^
 -DCMAKE_CONFIGURATION_TYPES:STRING="Release"
 
 REM -DSWIG_EXECUTABLE="C:/SDKs/swigwin-3.0.12/swig.exe" ^
 REM -Dtensorflow_WIN_CPU_SIMD_OPTIONS is unavailable now.
 
 REM Fails
-msbuild /p:Configuration=Release /p:Platform=x64 /m:8 tensorflow.sln /p:PreferredToolArchitecture=x64
+msbuild /p:Configuration=Release /p:Platform=x64 /m:1 tensorflow.sln /p:PreferredToolArchitecture=x64 > "%~dp0\msbuild_1st_log.txt" 2>&1
 
 cd /d %~dp0
 xcopy /E /I /Q /Y "tensorflow" "..\tensorflow\tensorflow"
 cd "..\tensorflow\tensorflow\contrib\cmake\build"
-msbuild /p:Configuration=Release /p:Platform=x64 /m:8 tensorflow.sln /p:PreferredToolArchitecture=x64
+msbuild /p:Configuration=Release /p:Platform=x64 /m:1 tensorflow.sln /p:PreferredToolArchitecture=x64 > "%~dp0\msbuild_2nd_log.txt" 2>&1
 
-ctest -C Release
+ctest -C Release > "%~dp0\ctest_log.txt" 2>&1
+
+cmake --build "." --target "ALL_BUILD" --config "Release" > "%~dp0\cmake_allbuild_log.txt" 2>&1
+cmake --build "." --target "INSTALL" --config "Release" > "%~dp0\cmake_install_log.txt" 2>&1
